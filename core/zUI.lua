b8f5bdd245dc53d67fd96e5b7357d8ca6e6e83d7
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-02-27 18:12:56 +0100

    small fix for BNToast

--- core/zUI.lua
+++ core/zUI.lua
@@ -164,9 +164,9 @@ HideQuickJoinToastButton:SetScript("OnEvent", function(self, event)
     if SettingsInitialized and event == "PLAYER_ENTERING_WORLD" and
         zUI_SavedSettings[PlayerIdentifier].HideQuickJoinToastButtonSetting then
         local status, error = pcall(function()
-            QuickJoinToastButton:ClearAllPoints()
-            QuickJoinToastButton:SetPoint("TOPLEFT", ChatFrame1Tab, "TOPLEFT",
-                                          0, 17)
+            -- QuickJoinToastButton:ClearAllPoints()
+            -- QuickJoinToastButton:SetPoint("TOPLEFT", ChatFrame1Tab, "TOPLEFT",
+            --                               0, 17)
             QuickJoinToastButton:Hide()
         end)
         if not status then zUI:Print(error) end
@@ -2018,25 +2018,3 @@ BankFrameMod:SetScript("OnEvent", function(self, event, changedBagSlotID)
         end
     end
 end)
-
--- function PrintWorldQuests(mapID)
---     local mapInfo = C_Map.GetMapInfo(mapID)
---     local zoneName = mapInfo and mapInfo.name or "Unknown"
-
---     local quests = C_TaskQuest.GetQuestsForPlayerByMapID(mapID)
---     if quests then
---         for i, info in ipairs(quests) do
---             local questID = info.questId
---             if QuestUtils_IsQuestWorldQuest(questID) then
---                 local title, _, _ = C_TaskQuest.GetQuestInfoByQuestID(questID)
---                 print("World quest found in " .. zoneName .. ": " .. questID .. " - " .. title)
---             end
---         end
---     end
-
---     local children = C_Map.GetMapChildrenInfo(mapID)
---     for i, info in ipairs(children) do
---         PrintWorldQuests(info.mapID)
---     end
--- end
-
